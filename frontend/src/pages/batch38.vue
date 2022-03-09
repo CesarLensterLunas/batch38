8cf568e662f2d88ae75f7f0ab08128c23bea10a1
CesarLensterLunas <lenster143@gmail.com>
2022-03-09 22:56:31 +0800

    Initial commit

---
+++ frontend/src/pages/batch38.vue
@@ -0,0 +1,18 @@
+<template>
+ <q-toolbar class="text-primary">
+      <q-btn flat round dense icon="menu" />
+      <q-toolbar-title>
+       Batch 38 to do app
+      </q-toolbar-title>
+      <q-btn flat round dense icon="more_vert" />
+    </q-toolbar>
+
+ <q-input filled v-model="text" label="Filled" placeholder="enter needs to be done" />
+<ul>
+<li>asd</li>
+<li>asd</li>
+<li>asd</li>
+<li>asd</li>
+<li>asd</li>
+</ul>
+</template>
